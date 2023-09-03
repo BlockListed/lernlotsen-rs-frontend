790c4c10cd8e2f91961e8e1fee344101f9c3a2f9
BlockListed <44610569+BlockListed@users.noreply.github.com>
2023-09-03 11:40:27 +0200

    fix form submissions

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -55,3 +55,61 @@ export const load: PageServerLoad = async ({ cookies, fetch }) => {
 		timeslots: timeslot_data
 	};
 };
+
+export const actions = {
+	create: async ({ request, fetch, cookies }) => {
+		check_auth(cookies);
+
+		const data = await request.formData();
+
+		let index = 0;
+
+		const subject = data.get('subject');
+		const weekday = data.get('weekday');
+		const time = data.get('time-start');
+		const time_end = data.get('time-end');
+		const date = data.get('date-start');
+		const date_end = data.get('date-end');
+		const timezone = data.get('timezone');
+		const students: string[] = [];
+
+		for (;;) {
+			const tmp = data.get(`student-${index}`);
+
+			if (tmp) {
+				students.push(tmp as string);
+			} else {
+				break;
+			}
+
+			index += 1;
+		}
+
+		const req = {
+			students: students.map((v) => {
+				return { name: v };
+			}),
+			subject: subject,
+			weekday: weekday,
+			time: {
+				start: `${time}:00`,
+				end: `${time_end}:00`
+			},
+			timerange: {
+				start: date,
+				end: date_end
+			},
+			timezone: timezone,
+		};
+
+		const resp = await fetch(`${env.API_URL}/timeslots`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(req)
+		});
+
+		await verify_status(resp);
+	}
+};
