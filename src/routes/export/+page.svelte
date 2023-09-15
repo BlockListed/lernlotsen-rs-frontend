c599483ff7c8e7341ae395b4548a123296ac40b0
BlockListed <44610569+BlockListed@users.noreply.github.com>
2023-09-15 19:51:45 +0200

    add text copy to clipboard

--- src/routes/export/+page.svelte
+++ src/routes/export/+page.svelte
@@ -4,11 +4,15 @@
 	import ExportSelect from '$lib/components/export_select.svelte';
 
 	export let data: PageData;
+
+	function toClipboard() {
+		navigator.clipboard.writeText(data.export);
+	}
 </script>
 
 <div class="md:w-2/3">
 	<ExportSelect timeslots={[]} />
 </div>
-<pre class="m-2 bg-slate-300 md:w-2/3">
+<pre on:click={toClipboard} class="m-2 bg-slate-300 md:w-2/3">
 {data.export}
 </pre>
