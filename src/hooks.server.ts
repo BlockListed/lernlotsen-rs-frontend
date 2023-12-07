7b4c9863681710bbbd7bafa861c534f21a3163f0
BlockListed <44610569+BlockListed@users.noreply.github.com>
2023-12-07 16:32:47 +0100

    fix cookie passthrough

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,7 +4,7 @@ export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
 	if (request.url.startsWith('http://127.0.0.1:8080/')) {
 		const cookies = event.request.headers.get('cookie');
 		if (cookies) {
-			request.headers.set('cookies', cookies);
+			request.headers.set('cookie', cookies);
 		}
 	}
 
