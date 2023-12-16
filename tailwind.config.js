63a6fcc4b4fc08fad41f99f625b2765c253e8b20
BlockListed <44610569+BlockListed@users.noreply.github.com>
2023-12-16 10:21:27 +0100

    remove useless tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		extend: {
-			colors: {
-				back: '#d5b3fd',
-				back2: '#bcc2e2',
-				box: '#a0cec5',
-				lowlight: '#7ed8a6',
-				hightlight: '#4ce283'
-			}
-		}
-	},
+	theme: {},
 	plugins: []
 };
