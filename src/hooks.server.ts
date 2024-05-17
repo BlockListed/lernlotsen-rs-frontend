e3d59f146b6bd840a176488fea3ffdc7b1f3a590
BlockListed <44610569+BlockListed@users.noreply.github.com>
2024-05-18 00:21:25 +0200

    turns out I dumb dumb

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,7 +2,7 @@ import type { HandleFetch } from '@sveltejs/kit';
 import { env } from '$env/dynamic/private';
 
 export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
-	if (request.url.startsWith('http://127.0.0.1:8080/') || requests.url.startsWith(env.API_URL)) {
+	if (request.url.startsWith('http://127.0.0.1:8080/') || request.url.startsWith(env.API_URL)) {
 		const cookies = event.request.headers.get('cookie');
 		if (cookies) {
 			request.headers.set('cookie', cookies);
